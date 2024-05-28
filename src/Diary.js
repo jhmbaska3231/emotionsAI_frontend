402851a14dbbbad02a86a40b24c37604eb56aedb
Henroacun <henroacun@gmail.com>
2024-05-29 02:05:43 +0800

    diary update

--- src/Diary.js
+++ src/Diary.js
@@ -33,8 +33,69 @@ const Diary = () => {
                     </ul>
                 </div>
                 <div className="diary-content">
-                    <h1>Diary</h1>
-                    {/* Add the content and functionality for the Diary page here */}
+                    <div className="ledger-box">
+                        <h2>Diary Ledger</h2>
+                        <div className="ledger-content">
+                            <div className="ledger-row">
+                                <div className="ledger-column date-column">
+                                    <h3>Date</h3>
+                                    <p>2024-05-01</p>
+                                </div>
+                                <div className="ledger-column diary-column">
+                                    <h3>Diary</h3>
+                                    <p>Today I started a new projectJKNASF NKJANFNIWAONIFOWANIOFNWAINFIAWNFIWANLFNjksanfawnfiuawnifwSAKJNFIPUAWFKSA FIAUWBFUIW</p>
+                                </div>
+                                <div className="ledger-column emotion-column">
+                                    <h3>Emotion Analysis</h3>
+                                    <p>HaJKBS FLKSBFIUAWBIFLNSLKAJLBFIWUBFKSAAOISFJNOIWABNIUFAWppy</p>
+                                </div>
+                            </div>
+                            <div className="ledger-row">
+                                <div className="ledger-column date-column">
+                                    <p>2024-05-02</p>
+                                </div>
+                                <div className="ledger-column diary-column">
+                                    <p>Work was challenging but rewarding...</p>
+                                </div>
+                                <div className="ledger-column emotion-column">
+                                    <p>Stressed</p>
+                                </div>
+                            </div>
+                            <div className="ledger-row">
+                                <div className="ledger-column date-column">
+                                    <p>2024-05-03</p>
+                                </div>
+                                <div className="ledger-column diary-column">
+                                    <p>Had a productive meeting with the team...</p>
+                                </div>
+                                <div className="ledger-column emotion-column">
+                                    <p>Motivated</p>
+                                </div>
+                            </div>
+                            <div className="ledger-row">
+                                <div className="ledger-column date-column">
+                                    <p>2024-05-04</p>
+                                </div>
+                                <div className="ledger-column diary-column">
+                                    <p>Completed the task ahead of schedule...</p>
+                                </div>
+                                <div className="ledger-column emotion-column">
+                                    <p>Accomplished</p>
+                                </div>
+                            </div>
+                            <div className="ledger-row">
+                                <div className="ledger-column date-column">
+                                    <p>2024-05-05</p>
+                                </div>
+                                <div className="ledger-column diary-column">
+                                    <p>Reflecting on the week's achievements...</p>
+                                </div>
+                                <div className="ledger-column emotion-column">
+                                    <p>Satisfied</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer />
